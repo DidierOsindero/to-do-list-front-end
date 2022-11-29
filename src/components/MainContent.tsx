3c3685a05333c3be56b1ec491c8505dc37e83ca0
Didier Osindero <didierosindero@gmail.com>
2022-11-29 08:49:43 +0000

    Add skeleton for POST to do handler

--- src/components/MainContent.tsx
+++ src/components/MainContent.tsx
@@ -15,6 +15,7 @@ export const MainContent = (): JSX.Element => {
   const [toDoArr, setToDoArr] = useState<IToDo[]>([]);
   const [inputText, setInputText] = useState<string>("");
 
+  //GET to dos from API
   const getToDoArr = async () => {
     console.log("getToDoArr function is running!")
     try {
@@ -25,6 +26,18 @@ export const MainContent = (): JSX.Element => {
     }
   }
 
+  //POST to dos to API
+  const postToDoArr = async (toDoText: string) => {
+    console.log("postToDoArr function is running!")
+    try {
+    const response = await axios.post(baseUrl+"/to-dos", toDoText);
+    setToDoArr(response.data); 
+    } catch (error){
+      console.error("Oops... there was an issue with your GET request: ", error)
+    }
+  }
+
+  //GET to dos stored on server DB on mount
   useEffect(() => {
     getToDoArr();
   },[])
